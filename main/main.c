359d0bbe799f0549343489164ac2188d2785aba7
jonathan <jonathan.willian321@gmail.com>
2026-05-27 23:57:02 -0300

    adicionado leitura de pressao, temperatura e umidade pelo BMP

--- main/main.c
+++ main/main.c
@@ -1,34 +1,53 @@
 #include <stdio.h>
-#include "driver/gpio.h"
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
+#include <freertos/FreeRTOS.h>
+#include <bmp280.h>
 
-#define LED_GPIO 5
+#define I2C_MASTER_SDA 21
+#define I2C_MASTER_SCL 22
 
-void app_main(void)
-{
-    // Configurar GPIO 5 como saída
-    gpio_config_t io_conf = {
-        .pin_bit_mask = (1ULL << LED_GPIO),
-        .mode = GPIO_MODE_OUTPUT,
-        .pull_up_en = GPIO_PULLUP_DISABLE,
-        .pull_down_en = GPIO_PULLDOWN_DISABLE,
-        .intr_type = GPIO_INTR_DISABLE,
-    };
-    gpio_config(&io_conf);
+bmp280_t bmp280;
 
-    printf("LED teste na GPIO 5 iniciado!\n");
+inline bool is_bme280p() {
+    return bmp280.id == BME280_CHIP_ID;
+}
 
-    // Loop infinito para piscar o LED
-    while(1) {
-        // LED ligado
-        gpio_set_level(LED_GPIO, 1);
-        printf("LED ligado\n");
-        vTaskDelay(1000 / portTICK_PERIOD_MS);  // 1 segundo
+void init_bmp280(void) {
+    printf("Procurando por bme/bmp280...\n");
 
-        // LED desligado
-        gpio_set_level(LED_GPIO, 0);
-        printf("LED desligado\n");
-        vTaskDelay(1000 / portTICK_PERIOD_MS);  // 1 segundo
+    bmp280_params_t params;
+    bmp280_init_default_params(&params);
+
+    esp_err_t err = bmp280_init_desc(&bmp280, BMP280_I2C_ADDRESS_0, 0, I2C_MASTER_SDA, I2C_MASTER_SCL);
+    ESP_ERROR_CHECK(err);
+
+    err = bmp280_init(&bmp280, &params);
+    ESP_ERROR_CHECK(err);
+
+    printf("BMP280: encontrado %s\n", is_bme280p() ? "BME280" : "BMP280");
+}
+
+void setup(void) {
+    ESP_ERROR_CHECK(i2cdev_init());
+    init_bmp280();
+}
+
+void app_main(void) {
+    setup();
+
+    float pressure, temperature, humidity;
+    for (;;) {
+        vTaskDelay(pdMS_TO_TICKS(500));
+
+        if (bmp280_read_float(&bmp280, &temperature, &pressure, &humidity) != ESP_OK) {
+            printf("Leitura do BMP280 falhou\n");
+            continue;
+        }
+
+        printf("Pressao: %.2f Pa, Temperatura: %.2f C", pressure, temperature);
+        if (is_bme280p()) {
+            printf(", Umidade: %.2f", humidity);
+        }
+
+        printf("\n");
     }
 }
