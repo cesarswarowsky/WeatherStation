d11c517f6423f8f13fac64aa6c66ba784b8c710b
silveiragabriel13 <silveiragabriel13@gmail.com>
2026-05-28 21:03:54 -0300

    button is pressed

--- main/main.c
+++ main/main.c
@@ -4,6 +4,8 @@
 
 #define I2C_MASTER_SDA 21
 #define I2C_MASTER_SCL 22
+#define BUTTON_GPIO GPIO_NUM_4
+
 
 bmp280_t bmp280;
 
@@ -29,11 +31,24 @@ void init_bmp280(void) {
 void setup(void) {
     ESP_ERROR_CHECK(i2cdev_init());
     init_bmp280();
+    
+
+    // 1. Configure the GPIO pin
+    gpio_config_t io_conf = {
+        .pin_bit_mask = (1ULL << BUTTON_GPIO),
+        .mode = GPIO_MODE_INPUT,
+        .pull_up_en = GPIO_PULLUP_ENABLE, // Use internal pull-up if button connects to GND
+        .pull_down_en = GPIO_PULLDOWN_DISABLE,
+        .intr_type = GPIO_INTR_DISABLE
+    };
+    gpio_config(&io_conf);
+
+    
 }
 
 void app_main(void) {
     setup();
-
+    int buckets = 0;
     float pressure, temperature, humidity;
     for (;;) {
         vTaskDelay(pdMS_TO_TICKS(500));
@@ -43,11 +58,16 @@ void app_main(void) {
             continue;
         }
 
-        printf("Pressao: %.2f Pa, Temperatura: %.2f C", pressure, temperature);
+       // printf("Pressao: %.2f Pa, Temperatura: %.2f C", pressure, temperature);
         if (is_bme280p()) {
-            printf(", Umidade: %.2f", humidity);
+        //    printf(", Umidade: %.2f", humidity);
         }
-
-        printf("\n");
+        int button_state = gpio_get_level(BUTTON_GPIO);
+        if (button_state == 0) {
+            printf("Button is pressed!\n");
+        } else {
+            printf("Button is released.\n");
+        }
+        //printf("\n");
     }
 }
